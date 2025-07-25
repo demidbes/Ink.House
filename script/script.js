f131d7a0ea78894113c14eae245fa7a09adad1e7
Дмитрий <dmitrii8155@icloud.com>
2025-07-25 14:11:33 +0300

    Adding a directory

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 
 document.addEventListener('DOMContentLoaded', function() {
     const burger = document.querySelector('.burger-button');
@@ -8,4 +8,57 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.toggle('active');
         menu.classList.toggle('menu-open');
     })
+});
+
+// Страны
+
+const catalogList = document.querySelector('.catalog__items');
+const catalogBtns = document.querySelectorAll('.catalog__button');
+
+const loadCatalog = (country) => {
+    catalogList.innerHTML = '';
+
+    fetch ('data/data.json')
+        .then((response) => {
+            return response.json();
+        })
+
+        .then((data) => {
+            for (const item of data) {
+                if (item.country == country) {
+                    // console.log(item)
+                    catalogList.innerHTML += `
+
+                        <li class="catalog__item">
+                            <article class="product">
+                                <img src="${item.cover}" alt="" class="product__image">
+                                <div class="product__descritpion">
+                                    <span class="product__author">${item.author}</span>
+                                    <h3 class="product__title">${item.title}</h3>
+                                    <span class="product__props">${item.props}</span>
+                                    <span class="product__price">${item.price}</span>
+                                </div>
+                                <button class="product__button">В корзину</button>
+                            </article>
+                        </li>
+
+                    `
+                }
+            }
+        })
+}
+
+loadCatalog('France');
+
+catalogBtns.forEach(el => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    const country = e.currentTarget.dataset.country;
+
+    catalogBtns.forEach(el => { el.classList.remove('catalog__button--active'); });
+
+    e.currentTarget.classList.add('catalog__button--active');
+
+    loadCatalog(country);
+  }); 
 });
